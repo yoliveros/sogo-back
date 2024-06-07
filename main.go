1daebf111f913b2800a9c50c44a4f6d3327fa069
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-06-06 20:57:59 -0400

    feat: mariadb connection added

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/yoliveros/sogo-back/middleware"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	router := http.NewServeMux()
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/sogo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	users, err := db.Query("CALL sp_get_users()")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for users.Next() {
+		var id string
+		var name string
+		var last_name string
+		var email string
+		var password string
+		var created_at string
+		var updated_at string
+		if err := users.Scan(&id, &name, &last_name, &email, &password, &created_at, &updated_at); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, name, email, password, created_at, updated_at)
+	}
 
 	loadRouters(router)
 
