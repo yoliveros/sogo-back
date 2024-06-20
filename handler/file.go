5c83015fa2f88776e4c5f7165330265935966423
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-06-19 21:53:07 -0400

    feat: nose

--- handler/file.go
+++ handler/file.go
@@ -10,17 +10,18 @@ import (
 )
 
 type File struct {
-	ID       string `json:"id"`
-	ParentID string `json:"parent_id"`
-	Name     string `json:"name"`
+	ID       string      `json:"id"`
+	ParentID interface{} `json:"parent_id"`
+	Name     string      `json:"name"`
+	IsFile   bool        `json:"is_file"`
 }
 
 func (h *File) GetFiles(w http.ResponseWriter, r *http.Request) {
-
 	rows, err := db.DB.Query("CALL sp_get_files()")
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 		return
 	}
 	defer rows.Close()
@@ -29,10 +30,11 @@ func (h *File) GetFiles(w http.ResponseWriter, r *http.Request) {
 
 	for rows.Next() {
 		file := File{}
-		err = rows.Scan(&file.ID, &file.ParentID, &file.Name)
+		err = rows.Scan(&file.ID, &file.ParentID, &file.Name, &file.IsFile)
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
 			return
 		}
 
@@ -46,20 +48,15 @@ func (h *File) GetFiles(w http.ResponseWriter, r *http.Request) {
 func (h *File) Upload(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(10 << 20)
 
-	var algo interface{}
-	algo = "hola"
-	log.Println(algo)
-
 	var parent_id interface{}
-
 	file, handle, err := r.FormFile("file")
 	parent_id = r.FormValue("parent_id")
-
-	log.Println(parent_id)
+	is_file := r.FormValue("is_file")
 
 	if err != nil {
 		log.Println("Error Retrieving the File: ", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 		return
 	}
 	defer file.Close()
@@ -74,6 +71,7 @@ func (h *File) Upload(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("Temp file creation error: ", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 		return
 	}
 	defer tempFile.Close()
@@ -82,6 +80,7 @@ func (h *File) Upload(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("File reading error: ", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 		return
 	}
 
@@ -89,20 +88,57 @@ func (h *File) Upload(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println("File writing error: ", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 		return
 	}
 
 	if parent_id == "" {
-		parent_id = &parent_id
+		parent_id = nil
 	}
 
-	_, err = db.DB.Exec("CALL sp_insert_files(?, ?)", parent_id, handle.Filename)
+	_, err = db.DB.Exec("CALL sp_insert_files(?, ?, ?)", parent_id, handle.Filename, is_file)
 	if err != nil {
 		log.Println("File writing error: ", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("File uploaded successfully"))
+}
+
+func (h *File) CreateFolder(w http.ResponseWriter, r *http.Request) {
+	file := File{}
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	err = json.Unmarshal(body, &file)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	_, err = os.Stat(file.Name)
+	if err != nil {
+		os.Mkdir(file.Name, 0755)
+	}
+
+	_, err = db.DB.Exec("CALL sp_insert_files(?, ?, ?)", file.ParentID, file.Name, file.IsFile)
+	if err != nil {
+		log.Println("File writing error: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Folder created successfully"))
 }
