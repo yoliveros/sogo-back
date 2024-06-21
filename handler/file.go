fdd7ad00b1cca9a7f47dc853d895250a8f6b5f93
yoliveros <28690224+yoliveros@users.noreply.github.com>
2024-06-20 20:29:04 -0400

    feat: done

--- handler/file.go
+++ handler/file.go
@@ -9,6 +9,8 @@ import (
 	"sogo-back/db"
 )
 
+const TEMP_FOLDER = "temp-files"
+
 type File struct {
 	ID       string      `json:"id"`
 	ParentID interface{} `json:"parent_id"`
@@ -31,6 +33,10 @@ func (h *File) GetFiles(w http.ResponseWriter, r *http.Request) {
 	for rows.Next() {
 		file := File{}
 		err = rows.Scan(&file.ID, &file.ParentID, &file.Name, &file.IsFile)
+		if file.ParentID != nil {
+			file.ParentID = string(file.ParentID.([]uint8))
+		}
+
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusInternalServerError)
@@ -45,13 +51,19 @@ func (h *File) GetFiles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(files)
 }
 
+func (h *File) createTempFolder() {
+	_, err := os.Stat(TEMP_FOLDER)
+	if err != nil {
+		os.Mkdir(TEMP_FOLDER, 0755)
+	}
+}
+
 func (h *File) Upload(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(10 << 20)
 
 	var parent_id interface{}
 	file, handle, err := r.FormFile("file")
 	parent_id = r.FormValue("parent_id")
-	is_file := r.FormValue("is_file")
 
 	if err != nil {
 		log.Println("Error Retrieving the File: ", err)
@@ -61,13 +73,10 @@ func (h *File) Upload(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	_, err = os.Stat("temp-files")
-	if err != nil {
-		os.Mkdir("temp-files", 0755)
-	}
+	h.createTempFolder()
 
 	// Create a temporary file within our temp-images directory
-	tempFile, err := os.CreateTemp("temp-files", "upload-.*"+handle.Filename)
+	tempFile, err := os.CreateTemp(TEMP_FOLDER, "upload-.*"+handle.Filename)
 	if err != nil {
 		log.Println("Temp file creation error: ", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -96,7 +105,7 @@ func (h *File) Upload(w http.ResponseWriter, r *http.Request) {
 		parent_id = nil
 	}
 
-	_, err = db.DB.Exec("CALL sp_insert_files(?, ?, ?)", parent_id, handle.Filename, is_file)
+	_, err = db.DB.Exec("CALL sp_insert_files(?, ?, ?)", parent_id, handle.Filename, true)
 	if err != nil {
 		log.Println("File writing error: ", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -126,9 +135,11 @@ func (h *File) CreateFolder(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = os.Stat(file.Name)
+	h.createTempFolder()
+
+	_, err = os.Stat(TEMP_FOLDER + "/" + file.Name)
 	if err != nil {
-		os.Mkdir(file.Name, 0755)
+		os.Mkdir(TEMP_FOLDER+"/"+file.Name, 0755)
 	}
 
 	_, err = db.DB.Exec("CALL sp_insert_files(?, ?, ?)", file.ParentID, file.Name, file.IsFile)
